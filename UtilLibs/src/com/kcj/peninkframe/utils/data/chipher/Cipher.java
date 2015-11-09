aff8f4f1f8761085c71582c5e4b8bac8e574e3cb
KouChengjian <hui793114465@163.com>
2015-11-09 16:14:52 +0800

    2015-11-9 更新lite开源库

--- UtilLibs/src/com/kcj/peninkframe/utils/data/chipher/Cipher.java
+++
@@ -1,9 +0,0 @@
-package com.kcj.peninkframe.utils.data.chipher;
-
-/**
- * @author MaTianyu
- * @date 14-7-31
- */
-public abstract class Cipher implements Encrypt,Decrypt{
-
-}
--- UtilLibs/src/com/kcj/peninkframe/utils/data/chipher/Encrypt.java
+++
@@ -1,9 +0,0 @@
-package com.kcj.peninkframe.utils.data.chipher;
-
-/**
- * @author MaTianyu
- * @date 14-7-31
- */
-public interface Encrypt {
-    public byte[] encrypt(byte[] res);
-}
